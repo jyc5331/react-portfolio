7cffa38fe42812c1f8a0d17cf92a0d816bcb73bd
jyc5331 <jcosgrovecoding@gmail.com>
2021-04-01 12:39:10 -0400

    navbar included, hrefs not working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React from "react";
 import PortfolioContainer from "./components/PortfolioContainer";
+import { Grommet } from "grommet";
 
-const App = () => <PortfolioContainer />;
+const theme = {
+  global: {
+    font: {
+      family: "Roboto",
+      size: "16px",
+      height: "18px",
+    },
+  },
+};
+
+const App = () => (
+  <Grommet theme={theme}>
+    <PortfolioContainer />
+  </Grommet>
+);
 
 export default App;
--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,4 +1,14 @@
 import React from "react";
+import {
+  Anchor,
+  Box,
+  Grommet,
+  Header,
+  Nav,
+  Menu,
+  ResponsiveContext,
+} from "grommet";
+import { grommet } from "grommet/themes";
 
 function NavTabs(props) {
   const tabs = ["About", "Portfolio", "Resume", "Contact"];
@@ -20,6 +30,36 @@ function NavTabs(props) {
       ))}
     </ul>
   );
+  // <Grommet theme={grommet}>
+  //   <Header background="dark-1" pad="medium">
+  //     <Box direction="row" align="center" gap="small">
+  //       Joseph Cosgrove{" "}
+  //     </Box>
+  //     <ResponsiveContext.Consumer>
+  //       {(responsive) =>
+  //         responsive === "small" ? (
+  //           <Menu
+  //             label="Click me"
+  //             items={[
+  //               { label: "About", onClick: () => {} },
+  //               { label: "Portfolio", onClick: () => {} },
+  //               { label: "Resume", onClick: () => {} },
+  //               { label: "Contact", onClick: () => {} },
+  //             ]}
+  //           />
+  //         ) : (
+  //           <Nav direction="row">
+  //             <Anchor href="#about" label="About" />
+  //             <Anchor href="#portfolio" label="Portfolio" />
+  //             <Anchor href="#resume" label="Resume" />
+  //             <Anchor href="#contact" label="Contact" />
+  //           </Nav>
+  //         )
+  //       }
+  //     </ResponsiveContext.Consumer>
+  //   </Header>
+  // </Grommet>
+  // );
 }
 
 export default NavTabs;
