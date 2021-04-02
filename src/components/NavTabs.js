c4e24e6ac95c7f4e1839d09aceb7c59d5cf0a478
jyc5331 <jcosgrovecoding@gmail.com>
2021-04-02 09:56:53 -0400

    portfolio and nav bar transitioned to Grommet

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -11,55 +11,71 @@ import {
 import { grommet } from "grommet/themes";
 
 function NavTabs(props) {
-  const tabs = ["About", "Portfolio", "Resume", "Contact"];
+  // const tabs = ["About", "Portfolio", "Resume", "Contact"];
   return (
-    <ul className="nav nav-tabs">
-      <h2>Joseph Cosgrove</h2>
-      {tabs.map((tab) => (
-        <li className="nav-item" key={tab}>
-          <a
-            href={"#" + tab.toLowerCase()}
-            onClick={() => props.handlePageChange(tab)}
-            className={
-              props.currentPage === tab ? "nav-link active" : "nav-link"
-            }
-          >
-            {tab}
-          </a>
-        </li>
-      ))}
-    </ul>
+    //   <ul className="nav nav-tabs">
+    //     <h2>Joseph Cosgrove</h2>
+    //     {tabs.map((tab) => (
+    //       <li className="nav-item" key={tab}>
+    //         <a
+    //           href={"#" + tab.toLowerCase()}
+    //           onClick={() => props.handlePageChange(tab)}
+    //           className={
+    //             props.currentPage === tab ? "nav-link active" : "nav-link"
+    //           }
+    //         >
+    //           {tab}
+    //         </a>
+    //       </li>
+    //     ))}
+    //   </ul>
+    // );
+    <Grommet theme={grommet}>
+      <Header background="dark-1" pad="medium">
+        <Box direction="row" align="center" gap="small">
+          Joseph Cosgrove{" "}
+        </Box>
+        <ResponsiveContext.Consumer>
+          {(responsive) =>
+            responsive === "small" ? (
+              <Menu
+                label="Click me"
+                items={[
+                  { label: "About", onClick: () => {} },
+                  { label: "Portfolio", onClick: () => {} },
+                  { label: "Resume", onClick: () => {} },
+                  { label: "Contact", onClick: () => {} },
+                ]}
+              />
+            ) : (
+              <Nav direction="row">
+                <Anchor
+                  href="#about"
+                  label="About"
+                  onClick={() => props.handlePageChange("About")}
+                />
+                <Anchor
+                  href="#portfolio"
+                  label="Portfolio"
+                  onClick={() => props.handlePageChange("Portfolio")}
+                />
+                <Anchor
+                  href="#resume"
+                  label="Resume"
+                  onClick={() => props.handlePageChange("Resume")}
+                />
+                <Anchor
+                  href="#contact"
+                  label="Contact"
+                  onClick={() => props.handlePageChange("Contact")}
+                />
+              </Nav>
+            )
+          }
+        </ResponsiveContext.Consumer>
+      </Header>
+    </Grommet>
   );
-  // <Grommet theme={grommet}>
-  //   <Header background="dark-1" pad="medium">
-  //     <Box direction="row" align="center" gap="small">
-  //       Joseph Cosgrove{" "}
-  //     </Box>
-  //     <ResponsiveContext.Consumer>
-  //       {(responsive) =>
-  //         responsive === "small" ? (
-  //           <Menu
-  //             label="Click me"
-  //             items={[
-  //               { label: "About", onClick: () => {} },
-  //               { label: "Portfolio", onClick: () => {} },
-  //               { label: "Resume", onClick: () => {} },
-  //               { label: "Contact", onClick: () => {} },
-  //             ]}
-  //           />
-  //         ) : (
-  //           <Nav direction="row">
-  //             <Anchor href="#about" label="About" />
-  //             <Anchor href="#portfolio" label="Portfolio" />
-  //             <Anchor href="#resume" label="Resume" />
-  //             <Anchor href="#contact" label="Contact" />
-  //           </Nav>
-  //         )
-  //       }
-  //     </ResponsiveContext.Consumer>
-  //   </Header>
-  // </Grommet>
-  // );
 }
 
 export default NavTabs;
